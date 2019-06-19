ecc7021dbdb5a662f5ab5b55ab92e043aae721bd
Robert Simon <rsimon5@gmu.edu>
2019-06-19 16:49:37 -0400

    adding file

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, jsonify, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+mongo = PyMongo(app)
+
+
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+
+
+@app.route("/scrape")
+def scrape():
+    mars = mongo.db.mars
+    mars_scrape = scrape_mars.scrape()
+    mars.update(
+        {},
+        mars_scrape,
+        upsert=True
+    )
+    return "Scraped is complete"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
